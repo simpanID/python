be68cff813e05641c53f7ecd35ff709b5a87ef29
simpanID <teamsansekerta@gmail.com>
2020-11-14 17:39:10 +0700

    main

--- Part 01/class-character.py
+++ Part 01/class-character.py
@@ -13,6 +13,5 @@ class Character():
 
 character_pertama = Character('Son Goku',89,100)
 print(character_pertama.info())
-
 total_spesial = character_pertama.spesial(3)
 print('Power Spesial : '+str(total_spesial))
