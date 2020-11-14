48b1c8f93707cbf33ca3d5e44baa326d0599f7eb
simpanID <teamsansekerta@gmail.com>
2020-11-14 17:47:36 +0700

    main

--- Part 01/class-character.py
+++ Part 01/class-character.py
@@ -3,16 +3,15 @@ class Character():
         self.name = name
         self.power = power
         self.healt = healt
-        
+    
     def info(self):
         return 'Name : '+self.name+'\nPower : '+str(self.power)+'\nHealt : '+str(self.healt)
-
+    
     def spesial(self,count):
         power_spesial = self.power * count
         return power_spesial
 
 character_pertama = Character('Son Goku',89,100)
 print(character_pertama.info())
-
 total_spesial = character_pertama.spesial(3)
 print('Power Spesial : '+str(total_spesial))
