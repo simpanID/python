846a9b473d509521e0b5da52334c2d29e3c71a75
simpanID <teamsansekerta@gmail.com>
2020-11-16 14:48:23 +0700

    Penomoran menggunakan FOR

---
+++ Part 01/belajar-class/index-perurutan.py
@@ -0,0 +1,5 @@
+character = ['songoku','Bezita']
+i = 0
+for characters in character:
+    print(str(i)+'.' + characters)
+    i+=1
--- Part 01/belajar-class/main.py
+++ Part 01/belajar-class/main.py
@@ -2,13 +2,18 @@ from character import Character
 
 character1 = Character('Son Goku',100,90)
 print(character1.print())
-spesial = character1.spesial(5)
+spesial = character1.spesial(3)
 print('Spesial : '+str(spesial))
 
-character2 = Character('Son Gohan',100,83)
+character2 = Character('Son Goten',100,80)
 print(character2.print())
 
-character3 = Character('Bezita',100,89)
+character3 = Character('Son Gohan',100,82)
 print(character3.print())
-spesial = character3.spesial(4)
-print('Spesial : '+str(spesial))
+
+characters = [character1,character2,character3]
+index = 0
+for character in characters:
+    print(str(index)+'.'+character.print())
+    index +=1
+
