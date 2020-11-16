3c30ccb0bc890282e88b0b8abac840f0b80672d0
simpanID <teamsansekerta@gmail.com>
2020-11-16 14:24:45 +0700

    perubahan import agar lebih mudah

--- Part 01/belajar-class/main.py
+++ Part 01/belajar-class/main.py
@@ -1,14 +1,14 @@
-import character
+from character import Character
 
-character1 = character.Character('Son Goku',100,90)
+character1 = Character('Son Goku',100,90)
 print(character1.print())
 spesial = character1.spesial(5)
 print('Spesial : '+str(spesial))
 
-character2 = character.Character('Son Gohan',100,83)
+character2 = Character('Son Gohan',100,83)
 print(character2.print())
 
-character3 = character.Character('Bezita',100,89)
+character3 = Character('Bezita',100,89)
 print(character3.print())
 spesial = character3.spesial(4)
 print('Spesial : '+str(spesial))
